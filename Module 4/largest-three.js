45781580b1e9f857933b7dda1f79acf5f0b693e0
Dibya Jyoti Dhar <dibya.dhar@northsouth.edu>
2026-09-09 23:57:30 +0600

    Module 4 Complete

--- Module 4/largest-three.js
+++ Module 4/largest-three.js
@@ -7,6 +7,9 @@ const rl = readline.createInterface({
 rl.question("Enter first number: ", (num1) => {
     rl.question("Enter second number: ", (num2) => {
         rl.question("Enter third number: ", (num3) => {
+            num1 = Number(num1);
+            num2 = Number(num2);
+            num3 = Number(num3);
             if(num1 >= num2 && num1 >= num3) {
                 console.log(`${num1} is the largest number.`);
             } else if(num2 >= num1 && num2 >= num3) {
